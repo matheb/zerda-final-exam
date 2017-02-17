c60c782e7a46f273d102caf437860c96697d7643
matheb <mathebogi@gmail.com>
2017-02-17 10:05:45 +0100

    one more step

--- exam.js
+++ exam.js
@@ -19,10 +19,9 @@ console.log(sumLetters('d uck'));
 //20. task: Create a function that takes a list of numbers and returns the second biggest element from it
 
 function secondBiggest(lista) {
-  var current;
   console.log(lista.length);
   for (var j = 0; j < lista.length ; j++) {
-    for (var i = 0, current; i < lista.length ; i++) {
+    for (var i = 0, current; i < lista.length-1; i++) {
       if (lista[i] > lista[i + 1]) {
         current = lista[i + 1];
         console.log(current);
